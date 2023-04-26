a8b2e6bb55d0991f37e725c7b10e254004235e19
Shreya00601 <129888978+Shreya00601@users.noreply.github.com>
2023-04-26 11:58:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,8 @@ try:
    else:
       back_from_function = get_fruityvice_data(fruit_choice)   
       streamlit.dataframe(back_from_function)  
-except URLError as e:
+
+    except URLError as e:
    streamlit.error()
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
