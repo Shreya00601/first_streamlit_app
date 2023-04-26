71e6d847aa042ee2d6b4e468ee6d8186b8e8abac
Shreya00601 <129888978+Shreya00601@users.noreply.github.com>
2023-04-26 12:21:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response (Lesson 9)
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json()) #just writes the data to the screen # This line was suppose to be deleted
@@ -31,5 +33,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+
 
 
